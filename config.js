3ef25dffd8a996eb51c911d3589fa061189c5685
KorobuBotz <107738150+Prasbot1@users.noreply.github.com>
2022-08-28 20:17:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 896-6674-7744'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
@@ -63,7 +63,7 @@ global.author = data.owner;
 
 //=========== Requirements ==========//  
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'ʙy ᴀɢᴜᴢ ꜰᴀᴍɪʟɪᴀ'
+        global.colong2 = 'ʙy Praszz ꜰᴀᴍɪʟɪᴀ'
 global.img = pickRandom(waifu)
 global.fla = pickRandom(flaaa)
 
